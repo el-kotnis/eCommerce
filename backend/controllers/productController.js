7365ca6af6d563256d9a8181733a33b8d2190fc5
Kshitij Kotnis <kkotnis13@gmail.com>
2022-01-29 18:52:39 +0530

    commit 5

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,15 @@
+const Products = require('../models/product')
+
+//create new product -> /api/v1/product/new
+exports.newProduct= async(req,res,next)=>{
+    const product = await Products.create(req.body)
+
+    res.status(201).json({
+        success:true,
+        product
+    })
+}
+
 exports.getProducts = (req,res,next)=>{
     res.status(200).json({
         success:true,
