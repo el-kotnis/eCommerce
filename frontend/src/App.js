3362185e7dcab419d4dc97db6b067df3219bbfeb
Kshitij Kotnis <kkotnis13@gmail.com>
2022-02-07 01:14:25 +0530

    commit 21

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
